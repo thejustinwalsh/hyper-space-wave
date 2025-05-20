e166fbd520c4c9440a786905f467d919d53dfac4
Justin Walsh <contact.me@thejustinwalsh.com>
2025-05-19 21:34:07 -0400

    fix: bug on instance effect, isAlive required

--- packages/core/src/hooks/useInstance.ts
+++ packages/core/src/hooks/useInstance.ts
@@ -3,21 +3,21 @@ import {useEffect} from 'react';
 import {useRef} from 'react';
 import {Instance} from '../state/traits';
 import {Entity} from 'koota';
-import {useTrait} from 'koota/react';
 import {Container} from 'pixi.js';
 
 export function useInstance<T extends Container>(entity: Entity) {
-  const instance = useTrait(entity, Instance);
   const ref = useRef<T>(null);
+
   useEffect(() => {
-    if (ref.current && !instance) {
+    if (ref.current && entity.isAlive() && !entity.has(Instance)) {
       entity.add(Instance({ref: ref.current}));
     }
     return () => {
-      if (instance) {
+      if (entity.isAlive() && entity.has(Instance)) {
         entity.remove(Instance);
       }
     };
-  }, [entity, instance]);
+  }, [entity]);
+
   return ref;
 }
